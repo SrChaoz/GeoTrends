55d7c88974dfaf5e24d1a46ab573f1e746e4a95e
SrChaoz <mateorubencho@gmail.com>
2025-07-21 14:39:28 -0500

    Docker Files Update

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,6 +5,10 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 import './index.css';
 
+// Establecer el título dinámicamente usando variables de entorno
+const appName = import.meta.env.VITE_APP_NAME || 'GeoTrends';
+document.title = `${appName}`;
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
